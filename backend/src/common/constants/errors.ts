afecb20fec6ca1e7584753e8c3be0cf6d80679bd
Anton Stolbetskyi <anton.sto02@gmail.com>
2023-02-04 15:17:29 +0200

    updating error messages

--- backend/src/common/constants/errors.ts
+++ backend/src/common/constants/errors.ts
@@ -1,6 +1,6 @@
 export const AppError = {
   USER_EXIST: 'User with this email already exist',
-  EMAIL_TAKEN: 'Email is already taken',
-  USER_NOT_EXIST: 'User with this email not exist',
+  EMAIL_TAKEN: 'Email address is already taken',
+  USER_NOT_EXIST: 'User with this email address does not exist',
   WRONG_DATA: 'Wrong data',
 };
