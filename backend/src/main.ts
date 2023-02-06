fd188f3b79f72dbf5d4f9c15f2f8e7020d056b6f
Anton Stolbetskyi <anton.sto02@gmail.com>
2023-02-06 16:06:56 +0200

    added custom body-parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.enableCors();
   await app.listen(5000);
 }
--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { CompanyModule } from '../companies/companies.module';
@@ -7,6 +12,7 @@ import { TokenModule } from '../token/token.module';
 import { SequelizeConfigService } from '../../config/sequilizeConfig.service';
 import { databaseConfig } from '../../config/configuration';
 import { AuthModule } from '../auth/auth.module';
+import { BodyParserMiddleware } from 'src/common/middleware/body-parser';
 
 @Module({
   imports: [
@@ -23,4 +29,13 @@ import { AuthModule } from '../auth/auth.module';
     TokenModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(BodyParserMiddleware)
+      .forRoutes(
+        { path: '*', method: RequestMethod.POST },
+        { path: '*', method: RequestMethod.PATCH },
+      );
+  }
+}
