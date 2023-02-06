6453c11a752d4886e99b498b8ecc6f3a3d3dc003
Anton Stolbetskyi <anton.sto02@gmail.com>
2023-02-06 16:07:55 +0200

    companies reducer fix

--- frontend/src/reducers/companies.js
+++ frontend/src/reducers/companies.js
@@ -2,6 +2,8 @@ import {
   DELETE_COMPANY_SUCCESS,
   GET_COMPANIES_SUCCESS,
   GET_COMPANIES_ADMIN_SUCCESS,
+  LOG_OUT,
+  DELETE_PROFILE_SUCCESS,
 } from '../actions/types'
 
 const initialState = []
@@ -14,6 +16,9 @@ export const companiesReducer = (state = initialState, action) => {
       return state.filter((company) => +company.id !== +action.payload)
     case GET_COMPANIES_ADMIN_SUCCESS:
       return action.payload
+    case LOG_OUT:
+    case DELETE_PROFILE_SUCCESS:
+      return initialState
     default:
       return state
   }
